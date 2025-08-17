a63d64d94c2e245087772f7d689bc70ae36a4563
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-08-17 19:39:52 +0300

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,33 @@
 'use strict';
 
-const food = {
-  Drink: {
-    Wine: {},
-    Schnaps: {},
-  },
-
-  Fruit: {
-    Red: {
-      Cherry: {},
-      Strawberry: {},
-    },
-    Yellow: {
-      Banana: {},
-      Pineapple: {},
-    },
-  },
-};
-
-const tree = document.querySelector('#tree');
-
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  // eslint-disable-next-line curly
+  if (!element) return;
+
+  const buildList = (obj) => {
+    const ul = document.createElement('ul');
+
+    Object.keys(obj).forEach((key) => {
+      const li = document.createElement('li');
+      // eslint-disable-next-line padding-line-between-statements
+      li.textContent = key;
+
+      const child = obj[key];
+
+      if (child && typeof child === 'object' && Object.keys(child).length > 0) {
+        li.appendChild(buildList(child));
+      }
+
+      ul.appendChild(li);
+    });
+
+    return ul;
+  };
+
+  element.innerHTML = '';
+  element.appendChild(buildList(data));
 }
 
-createTree(tree, food);
+if (typeof module !== 'undefined') {
+  module.exports = { createTree };
+}
